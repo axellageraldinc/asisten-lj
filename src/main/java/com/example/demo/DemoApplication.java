a77406daf315b8bc424a6781925b79188698846d
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-24 20:15:00 +0700

    - second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@LineMessageHandler
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@EventMapping
+	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> msg){
+		return new TextMessage("kowe ngirim : " + msg.getMessage().getText());
 	}
 }
