00656349931eee38185215967decee587d418b2d
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-25 15:32:18 +0700

    coba get title

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,13 @@ import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @LineMessageHandler
@@ -18,9 +23,26 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public String ConnectWebDteti(){
+		String title="";
+		try {
+			Document doc = Jsoup.connect("http://sarjana.jteti.ugm.ac.id/akademik").get();
+			title = doc.title();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return title;
+	}
+
 	@EventMapping
 	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> msg){
-		return new TextMessage("kowe ngirim teks : " + msg.getMessage().getText());
+		TextMessage replyMsg = null;
+		if (msg.getMessage().getText().equals("judul")){
+			replyMsg = new TextMessage(ConnectWebDteti());
+		} else{
+			replyMsg = new TextMessage("kowe ngirim : " + msg.getMessage().getText());
+		}
+		return replyMsg;
 	}
 
 	@EventMapping
