b7a47cb96cb0441d07d4890a71a31a8ddf61b454
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-09-27 13:46:06 +0700

    no message

--- src/main/java/com/example/demo/Controller/HandleTextMessageController.java
+++ src/main/java/com/example/demo/Controller/HandleTextMessageController.java
@@ -18,55 +18,55 @@ import java.util.Arrays;
 
 @LineMessageHandler
 public class HandleTextMessageController {
-    @EventMapping
-    public TemplateMessage handleCarousel(MessageEvent<TextMessageContent> msg){
-        TemplateMessage templateMessage = null;
-        if (msg.getMessage().getText().equals("carousel")){
-            CarouselTemplate carouselTemplate = new CarouselTemplate(
-                    Arrays.asList(
-                            new CarouselColumn(null, "hoge", "fuga", Arrays.asList(
-                                    new URIAction("Go to line.me",
-                                            "https://line.me"),
-                                    new URIAction("Go to line.me",
-                                            "https://line.me"),
-                                    new PostbackAction("Say hello1",
-                                            "hello こんにちは")
-                            )),
-                            new CarouselColumn(null, "hoge", "fuga", Arrays.asList(
-                                    new PostbackAction("言 hello2",
-                                            "hello こんにちは",
-                                            "hello こんにちは"),
-                                    new PostbackAction("言 hello2",
-                                            "hello こんにちは",
-                                            "hello こんにちは"),
-                                    new MessageAction("Say message",
-                                            "Rice=米")
-                            )),
-                            new CarouselColumn(null, "Datetime Picker", "Please select a date, time or datetime", Arrays.asList(
-                                    new DatetimePickerAction("Datetime",
-                                            "action=sel",
-                                            "datetime",
-                                            "2017-06-18T06:15",
-                                            "2100-12-31T23:59",
-                                            "1900-01-01T00:00"),
-                                    new DatetimePickerAction("Date",
-                                            "action=sel&only=date",
-                                            "date",
-                                            "2017-06-18",
-                                            "2100-12-31",
-                                            "1900-01-01"),
-                                    new DatetimePickerAction("Time",
-                                            "action=sel&only=time",
-                                            "time",
-                                            "06:15",
-                                            "23:59",
-                                            "00:00")
-                            ))
-                    ));
-            templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
-        }
-        return templateMessage;
-    }
+//    @EventMapping
+//    public TemplateMessage handleCarousel(MessageEvent<TextMessageContent> msg){
+//        TemplateMessage templateMessage = null;
+//        if (msg.getMessage().getText().equals("carousel")){
+//            CarouselTemplate carouselTemplate = new CarouselTemplate(
+//                    Arrays.asList(
+//                            new CarouselColumn(null, "hoge", "fuga", Arrays.asList(
+//                                    new URIAction("Go to line.me",
+//                                            "https://line.me"),
+//                                    new URIAction("Go to line.me",
+//                                            "https://line.me"),
+//                                    new PostbackAction("Say hello1",
+//                                            "hello こんにちは")
+//                            )),
+//                            new CarouselColumn(null, "hoge", "fuga", Arrays.asList(
+//                                    new PostbackAction("言 hello2",
+//                                            "hello こんにちは",
+//                                            "hello こんにちは"),
+//                                    new PostbackAction("言 hello2",
+//                                            "hello こんにちは",
+//                                            "hello こんにちは"),
+//                                    new MessageAction("Say message",
+//                                            "Rice=米")
+//                            )),
+//                            new CarouselColumn(null, "Datetime Picker", "Please select a date, time or datetime", Arrays.asList(
+//                                    new DatetimePickerAction("Datetime",
+//                                            "action=sel",
+//                                            "datetime",
+//                                            "2017-06-18T06:15",
+//                                            "2100-12-31T23:59",
+//                                            "1900-01-01T00:00"),
+//                                    new DatetimePickerAction("Date",
+//                                            "action=sel&only=date",
+//                                            "date",
+//                                            "2017-06-18",
+//                                            "2100-12-31",
+//                                            "1900-01-01"),
+//                                    new DatetimePickerAction("Time",
+//                                            "action=sel&only=time",
+//                                            "time",
+//                                            "06:15",
+//                                            "23:59",
+//                                            "00:00")
+//                            ))
+//                    ));
+//            templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
+//        }
+//        return templateMessage;
+//    }
     //TIDAK TERPAKAI LAGI
 //    GetInfoWebSarjana infoWebSarjana = new GetInfoWebSarjana();
 //
