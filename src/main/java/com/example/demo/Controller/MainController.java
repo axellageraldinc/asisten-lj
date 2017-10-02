f8baf0608e3eb7b6cb1e54b1cd749b175e42aebe
Axellageraldinc Adryamarthanino <axella.gerald@gmail.com>
2017-10-02 10:21:55 +0700

    no message

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -31,10 +31,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import retrofit2.Response;
 
 import javax.annotation.Resource;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.net.ssl.SSLException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -45,6 +46,9 @@ public class MainController {
 
     int status_waiting_game=0;
     List<String> playerList = new ArrayList<>();
+
+    private static final String api_key = "NlDLxQN3giFSs1AI899WNnZTtCuy-mt7";
+    private static final String api_secret = "3W95_8wUGxaiKvcmxDCEciBsTgkTl0E1";
 
     @Resource
     AsyncServices services;
@@ -99,11 +103,16 @@ public class MainController {
 
     public void handleImageContent(String replyToken, String id){
         try {
-            MessageContentResponse apiResponse = lineMessagingClient
+            Response<ResponseBody> response =
+                    LineMessagingServiceBuilder
+                    .create(AccessToken)
+                    .build()
                     .getMessageContent(id)
-                    .get();
-            InputStream isi = apiResponse.getStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(isi));
+                    .execute();
+            if(response.isSuccessful()){
+                ResponseBody content = response.body();
+                InputStream isi = content.byteStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(isi));
                 String line;
                 StringBuffer responsee = new StringBuffer();
                 while((line = rd.readLine()) != null) {
@@ -111,35 +120,131 @@ public class MainController {
                     responsee.append('\r');
                 }
                 rd.close();
-            System.out.println("API RESPONSE : " + apiResponse.toString());
-                System.out.println("Image Content : " + responsee.toString());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-//        try {
-//            Response<ResponseBody> response =
-//                    LineMessagingServiceBuilder
-//                    .create(AccessToken)
-//                    .build()
-//                    .getMessageContent(id)
-//                    .execute();
-//            if(response.isSuccessful()){
-//                ResponseBody content = response.body();
-//                InputStream isi = content.byteStream();
-//                BufferedReader rd = new BufferedReader(new InputStreamReader(isi));
-//                String line;
-//                StringBuffer responsee = new StringBuffer();
-//                while((line = rd.readLine()) != null) {
-//                    responsee.append(line);
-//                    responsee.append('\r');
-//                }
-//                rd.close();
-//                System.out.println("Image Content : " + responsee.toString());
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+//                File file = new File("src/res/AXELL.JPG");
+//                byte[] buff = getBytesFromFile(file);
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                int nRead;
+                byte[] data = new byte[1024];
+                while ((nRead = isi.read(data, 0, data.length)) != -1) {
+                    buffer.write(data, 0, nRead);
+                }
 
+                buffer.flush();
+                byte[] buff = buffer.toByteArray();
+                String url = "https://api-us.faceplusplus.com/facepp/v3/detect";
+                HashMap<String, String> map = new HashMap<>();
+                HashMap<String, byte[]> byteMap = new HashMap<>();
+                map.put("api_key", api_key);
+                map.put("api_secret", api_secret);
+                byteMap.put("image_file", buff);
+                try{
+                    byte[] bacd = post(url, map, byteMap);
+                    String str = new String(bacd);
+                    System.out.println(str);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private final static int CONNECT_TIME_OUT = 30000;
+    private final static int READ_OUT_TIME = 50000;
+    private static String boundaryString = getBoundary();
+    protected static byte[] post(String url, HashMap<String, String> map, HashMap<String, byte[]> fileMap) throws Exception {
+        HttpURLConnection conne;
+        URL url1 = new URL(url);
+        conne = (HttpURLConnection) url1.openConnection();
+        conne.setDoOutput(true);
+        conne.setUseCaches(false);
+        conne.setRequestMethod("POST");
+        conne.setConnectTimeout(CONNECT_TIME_OUT);
+        conne.setReadTimeout(READ_OUT_TIME);
+        conne.setRequestProperty("accept", "*/*");
+        conne.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundaryString);
+        conne.setRequestProperty("connection", "Keep-Alive");
+        conne.setRequestProperty("user-agent", "Mozilla/4.0 (compatible;MSIE 6.0;Windows NT 5.1;SV1)");
+        DataOutputStream obos = new DataOutputStream(conne.getOutputStream());
+        Iterator iter = map.entrySet().iterator();
+        while(iter.hasNext()){
+            Map.Entry<String, String> entry = (Map.Entry) iter.next();
+            String key = entry.getKey();
+            String value = entry.getValue();
+            obos.writeBytes("--" + boundaryString + "\r\n");
+            obos.writeBytes("Content-Disposition: form-data; name=\"" + key
+                    + "\"\r\n");
+            obos.writeBytes("\r\n");
+            obos.writeBytes(value + "\r\n");
+        }
+        if(fileMap != null && fileMap.size() > 0){
+            Iterator fileIter = fileMap.entrySet().iterator();
+            while(fileIter.hasNext()){
+                Map.Entry<String, byte[]> fileEntry = (Map.Entry<String, byte[]>) fileIter.next();
+                obos.writeBytes("--" + boundaryString + "\r\n");
+                obos.writeBytes("Content-Disposition: form-data; name=\"" + fileEntry.getKey()
+                        + "\"; filename=\"" + encode(" ") + "\"\r\n");
+                obos.writeBytes("\r\n");
+                obos.write(fileEntry.getValue());
+                obos.writeBytes("\r\n");
+            }
+        }
+        obos.writeBytes("--" + boundaryString + "--" + "\r\n");
+        obos.writeBytes("\r\n");
+        obos.flush();
+        obos.close();
+        InputStream ins = null;
+        int code = conne.getResponseCode();
+        try{
+            if(code == 200){
+                ins = conne.getInputStream();
+            }else{
+                ins = conne.getErrorStream();
+            }
+        }catch (SSLException e){
+            e.printStackTrace();
+            return new byte[0];
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buff = new byte[4096];
+        int len;
+        while((len = ins.read(buff)) != -1){
+            baos.write(buff, 0, len);
+        }
+        byte[] bytes = baos.toByteArray();
+        ins.close();
+        return bytes;
+    }
+    private static String getBoundary() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < 32; ++i) {
+            sb.append("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-".charAt(random.nextInt("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_".length())));
+        }
+        return sb.toString();
+    }
+    private static String encode(String value) throws Exception{
+        return URLEncoder.encode(value, "UTF-8");
+    }
+    public static byte[] getBytesFromFile(File f) {
+        if (f == null) {
+            return null;
+        }
+        try {
+            FileInputStream stream = new FileInputStream(f);
+            ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+            byte[] b = new byte[1000];
+            int n;
+            while ((n = stream.read(b)) != -1)
+                out.write(b, 0, n);
+            stream.close();
+            out.close();
+            return out.toByteArray();
+        } catch (IOException e) {
+        }
+        return null;
     }
 
     public void handleContent(String replyToken, Event event, TextMessageContent content){
