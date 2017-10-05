b8a5b328d5bb27c814c40b1c1463c47b53a1a051
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2017-10-05 21:25:36 +0700

    Setting up to deploy in tomcat server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import com.example.demo.Dao.MainDao;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.ExecutionException;
@@ -12,7 +14,12 @@ import java.util.concurrent.ExecutionException;
 @LineMessageHandler
 @EnableAsync
 @RestController
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		SpringApplication.run(DemoApplication.class, args);
